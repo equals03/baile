f7d5769bd03730de5dd6cc2f93ccb98f38ed53bc
Mike <equals.zero.three@gmail.com>
2025-09-21 16:14:20 +0200

    feat(installer): just some work on an installer
    not even sure I'm going to keep this...

--- lib/default.nix
+++ lib/default.nix
@@ -47,4 +47,78 @@ _: {
 
       lib = lib';
     };
+
+  mkInstaller = {
+    self,
+    pkgs,
+    lib ? pkgs.lib,
+  }: let
+    to-lines = lib.concatStringsSep "\n";
+    profiles = lib.filterAttrs (_: profile: profile.system == pkgs.system) (self.baileConfigurations or {});
+    profiles' =
+      lib.mapAttrsToList (
+        name: profile: ''["${lib.escapeShellArg name}"]="${lib.getExe profile.installer.program}"''
+      )
+      profiles;
+
+    text = ''
+      IFS=$'\n\t'
+      # ANSI colour codes
+      RED='\033[0;91m'
+      GREEN='\033[0;92m'
+      BLUE='\033[0;96m'
+      WHITE='\033[0;97m'
+      NO_COLOUR='\033[0m'
+
+      prog="$(basename "$0")"
+
+      msg()   { printf "''${WHITE}%s''${NO_COLOUR}\\n" "$*"; }
+      info()  { printf "''${BLUE}%s''${NO_COLOUR}\\n" "$*"; }
+      ok()    { printf "''${GREEN}%s''${NO_COLOUR}\\n" "$*"; }
+      warn()  { printf "''${YELLOW}%s''${NO_COLOUR}\\n" "$*"; }
+      fail()  { printf "''${RED}''${prog}: ERROR: %s [Line %s]''${NO_COLOUR}\\n" "$2" "''${1:-N/A}" >&2; exit "''${3:-1}"; }
+
+      trap 'fail "$LINENO" "Unexpected error. Exiting."' ERR INT
+
+      declare -A profiles=(
+        ${to-lines profiles'}
+      )
+
+      lookup_profile() {
+        local query="$1"
+        if [[ -v profiles[$query] ]]; then
+          echo "''${profiles[$query]}"
+        fi
+      }
+
+      info "baile will now attempt to determine the correct profile to install..."
+      for user in "$USER@$(hostname -f)" "$USER@$(hostname)" "$USER@$(hostname -s)" "$USER"; do
+        info "Searching for profile: $user"
+
+        profile=$(lookup_profile "$user")
+        if [[ $profile ]]; then
+          ok "Found profile: $profile"
+          info "Installing profile..."
+          $profile
+          ok "Profile installed!"
+
+          break
+        fi
+      done
+    '';
+
+    program = pkgs.writeShellApplication {
+      inherit text;
+      name = "baile";
+      runtimeInputs = with pkgs; [coreutils];
+      meta = {
+        description = "Baile bootstrap installer.";
+        platforms = with pkgs; [system];
+        maintainers = ["equals03"];
+      };
+    };
+  in {
+    inherit program;
+    type = "app";
+  };
 }
